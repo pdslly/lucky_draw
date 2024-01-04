ceaa54bbb1acaa7c7a0be9e3df36ce5427bb13d9
xiang <admin@ifront.net>
2024-01-04 15:20:10 +0800

    feature_xiang_01041520_新增环境配置

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,23 @@ import { defineConfig, loadEnv } from "vite";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import { createHtmlPlugin } from "vite-plugin-html";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
-  const { VITE_API_URL } = loadEnv(mode, process.cwd());
+  const { VITE_TITLE } = loadEnv(mode, process.cwd());
 
   return defineConfig({
     plugins: [
       vue(),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            title: VITE_TITLE,
+          },
+        },
+      }),
       AutoImport({
         resolvers: [ElementPlusResolver()],
       }),
